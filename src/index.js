d0a204232ece2cb6c89f69811ffaac21ff61ba39
ilia <ilyakut@gmail.com>
2020-05-31 23:44:57 +0300

    added payload

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ const reducer = (state = 0, action) => {
     case 'DEC':
       return state - 1;
 
+    case 'RND':
+      return state + action.payload;
+
     default:
       return state;
   }
@@ -23,6 +26,11 @@ document
 document.getElementById('dec')
   .addEventListener('click', () => {
     store.dispatch({type: 'DEC'})
+  });
+document.getElementById('rnd')
+  .addEventListener('click', () => {
+    const payload = Math.floor(Math.random()*10);
+    store.dispatch({type: 'RND', payload})
   });
 
 const update = ()  => {
