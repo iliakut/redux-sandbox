ef94eac5406389380bf3ef2afa959ba50c537e03
ilia <ilyakut@gmail.com>
2020-06-02 19:15:11 +0300

    added react component

---
+++ src/components/counter.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {bindActionCreators} from "redux";
+import {connect} from "react-redux";
+import * as actions from "../actions";
+
+const Counter = ({counter, inc, dec, rnd}) => {
+  return (
+    <div className="jumbotron">
+      <h2>{counter}</h2>
+      <button className="btn btn-primary btn-large" onClick={dec}>DEC</button>
+      <button className="btn btn-primary btn-large" onClick={inc}>INC</button>
+      <button className="btn btn-primary btn-large" onClick={rnd}>RND</button>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    counter: state
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
+  return {
+    inc,
+    dec,
+    rnd: () => {
+      const random = Math.floor(Math.random()*10);
+      rnd(random)
+    }
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
